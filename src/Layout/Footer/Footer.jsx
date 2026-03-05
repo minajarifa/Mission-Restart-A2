0cfdfda498306ae7bad56c7a0b21a10f9d37add9
Your Name <minajmiha02@gmail.com>
2026-03-05 07:57:22 +0600

    first commit

---
+++ src/Layout/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+export default function Footer() {
+  return (
+    <Footer>
+        {/* last Footer */}
+      <footer className="p-4 footer sm:footer-horizontal footer-center bg-base-300 text-base-content">
+        <aside>
+          <p>
+            Copyright © {new Date().getFullYear()} - All right reserved by ACME
+            Industries Ltd
+          </p>
+        </aside>
+      </footer>
+  
+    </Footer>
+  );
+}
