6a2d3a9109d97744aa35e7fdcf8d0f3a36b99875
Your Name <minajmiha02@gmail.com>
2026-03-05 16:19:03 +0600

    added json data in public folder

--- src/Layout/Banner/Banner.jsx
+++ src/Layout/Banner/Banner.jsx
@@ -1,50 +1,24 @@
-// export default function Banner() {
-//   return (
-//     <div>
-//       <div
-//         className="min-h-screen hero"
-//         style={{
-//           backgroundImage:
-//             "url(https://i.ibb.co.com/qQ5Gcrw/vector1.png)",
-//         }}
-//       >
-//         <div className="hero-overlay"></div>
-//         <div className="text-center hero-content text-neutral-content">
-//           <div className="max-w-md">
-//             <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-//             <p className="mb-5">
-//               Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-//               excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-//               et a id nisi.
-//             </p>
-//             <button className="btn btn-primary">Get Started</button>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
 export default function Banner() {
   return (
-    <div className="md:flex ">
-      <div>
-        {/* <img src="/public/vector1.png" alt="" className="bg-gradient-to-r from-[#632EE3] to-[#9F62F2] "/> */}
-       <div  className="flex bg-gradient-to-r from-[#632EE3] to-[#9F62F2] gap-5">
-         <img src="/public/vector1.png" alt="" className="" />
+    <div className="justify-center lg:flex">
+      <div className="flex bg-gradient-to-r from-[#632EE3] to-[#9F62F2] rounded-2xl m-10">
+        <img src="/public/vector1.png" alt="" className="" />
+        <div className="mt-20 text-center text-white">
+          <h1 className="font-bold">In-Progress</h1>
+          <h1>0</h1>
+        </div>
         <img
           src="/public/vector1.png"
           alt=""
-          className=" transform scale-x-[-1]"
+          className=" transform scale-x-[-1] "
         />
-       </div>
-        <h1 className="absolute font-bold -top-0 ">
-        {/* <h1 className="absolute font-bold left-5 top-10"> */}
-          Hello World
-        </h1>
       </div>
-      <div className="bg-gradient-to-r from-[#54CF68] to-[#00827A] flex">
+      <div className="bg-gradient-to-r from-[#54CF68] to-[#00827A] flex rounded-2xl m-10">
         <img src="/public/vector1.png" alt="" className="" />
+        <div className="mt-20 text-center text-white">
+          <h1 className="font-bold">Resolved</h1>
+          <h1>0</h1>
+        </div>
         <img
           src="/public/vector1.png"
           alt=""
