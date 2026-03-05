542136aee7febeac900a387620d51534ee776271
Your Name <minajmiha02@gmail.com>
2026-03-05 08:01:33 +0600

    added footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Outlet } from 'react-router-dom'
 import './App.css'
 import Header from './Layout/Header/Header'
+import Footer from './Layout/Footer/Footer'
 
 function App() {
   return (
     <>
     <Header></Header>
      <Outlet></Outlet>
+     <Footer></Footer>
     </>
   )
 }
--- src/Layout/Footer/Footer.jsx
+++ src/Layout/Footer/Footer.jsx
@@ -1,16 +1,52 @@
 export default function Footer() {
   return (
-    <Footer>
-        {/* last Footer */}
-      <footer className="p-4 footer sm:footer-horizontal footer-center bg-base-300 text-base-content">
+    <footer>
+      {/* first footer  */}
+      <div className="p-10 footer sm:footer-horizontal bg-base-200 text-base-content">
+        <nav>
+          <h6 className="footer-title">Services</h6>
+          <a className="link link-hover">Branding</a>
+          <a className="link link-hover">Design</a>
+          <a className="link link-hover">Marketing</a>
+          <a className="link link-hover">Advertisement</a>
+        </nav>
+        <nav>
+          <h6 className="footer-title">Company</h6>
+          <a className="link link-hover">About us</a>
+          <a className="link link-hover">Contact</a>
+          <a className="link link-hover">Jobs</a>
+          <a className="link link-hover">Press kit</a>
+        </nav>
+        <nav>
+          <h6 className="footer-title">Legal</h6>
+          <a className="link link-hover">Terms of use</a>
+          <a className="link link-hover">Privacy policy</a>
+          <a className="link link-hover">Cookie policy</a>
+        </nav>
+        <form>
+          <h6 className="footer-title">Newsletter</h6>
+          <fieldset className="w-80">
+            <label>Enter your email address</label>
+            <div className="join">
+              <input
+                type="text"
+                placeholder="username@site.com"
+                className="input input-bordered join-item"
+              />
+              <button className="btn btn-primary join-item">Subscribe</button>
+            </div>
+          </fieldset>
+        </form>
+      </div>
+      {/* last Footer */}
+      <div className="p-4 footer sm:footer-horizontal footer-center bg-base-300 text-base-content">
         <aside>
           <p>
             Copyright © {new Date().getFullYear()} - All right reserved by ACME
             Industries Ltd
           </p>
         </aside>
-      </footer>
-  
-    </Footer>
+      </div>
+    </footer>
   );
 }
