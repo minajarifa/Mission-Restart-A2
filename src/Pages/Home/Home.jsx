6fe4f5bae06fa257045bb36c444b64d757a1bb4d
Your Name <minajmiha02@gmail.com>
2026-03-08 21:17:14 +0600

    start

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,15 +3,15 @@ import Banner from "../../Layout/Banner/Banner";
 import TicketContainer from "../TicketContainer/TicketContainer";
 
 const fetchTicket = async () => {
-  const res = await fetch(`/public/ticket.json`)
+  const res = await fetch(`/ticket.json`)
   return res.json();
 };
 export default function Home() {
   const TicketPromise= fetchTicket()
   return (
-    <div>
+    <div className="text-center">
       <Banner></Banner>
-      <Suspense fallback={`Ticket data comming soon`}>
+      <Suspense fallback={<span className="loading loading-bars loading-xs text-9xl"></span>}>
         <TicketContainer TicketPromise={TicketPromise}></TicketContainer>
       </Suspense>
     </div>
--- src/Pages/TicketContainer/TicketContainer.jsx
+++ src/Pages/TicketContainer/TicketContainer.jsx
@@ -1,10 +1,39 @@
+import { use } from "react";
 
-import { use } from "react"
-
-export default function TicketContainer({TicketPromise}) {
-    const ticketData = use(TicketPromise)
-    console.log(ticketData)
+export default function TicketContainer({ TicketPromise }) {
+  const ticketData = use(TicketPromise);
+  const tickets = ticketData.tickets;
+  console.log(tickets);
   return (
-    <div>{ticketData?.length}</div>
-  )
+    <div className="">
+      {/* All ticket card */}
+      <div>
+        <h1>Customer Ticket</h1>
+        <div>
+          {/* start */}
+          <div className="shadow-sm card bg-base-100 w-96">
+            <div className="card-body">
+              <div className="flex justify-between">
+                <h2 className="font-bold card-title">
+                Card Title
+              </h2>
+                <span className="badge badge-secondary">NEW</span>
+              </div>
+              <p>
+                A card component has a figure, a body part, and inside body
+                there are title and actions parts
+              </p>
+              <div className="justify-between card-actions">
+                <div className="badge badge-outline">Fashion</div>
+                <div className="badge badge-outline">Products</div>
+              </div>
+            </div>
+          </div>
+          {/* end */}
+        </div>
+      </div>
+      {/* task status */}
+      <div></div>
+    </div>
+  );
 }
