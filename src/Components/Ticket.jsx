265d9b7edac622ddc45a88c903119c8b7f1cf486
Your Name <minajmiha02@gmail.com>
2026-03-09 08:21:08 +0600

    added ticket component

---
+++ src/Components/Ticket.jsx
@@ -0,0 +1,27 @@
+export default function Ticket({ ticket }) {
+  return (
+    <div key={ticket.id}>
+      <div>
+        {/* start */}
+        <div className="shadow-sm card bg-base-100 w-96">
+          <div className="card-body">
+            <div className="flex justify-between">
+              <h2 className="font-bold card-title">{ticket?.title}</h2>
+              <span className="badge badge-secondary">{ticket?.status}</span>
+            </div>
+            <p>{ticket?.description}</p>
+            <div className="justify-between card-actions">
+              <div className=" badge badge-outline">
+                <span>#{ticket?.id}</span> {ticket?.priority} priority
+              </div>
+              <div className=" badge badge-outline">
+                {ticket?.customer} <span>{ticket?.createdAt}</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* end */}
+      </div>
+    </div>
+  );
+}
--- src/Pages/TicketContainer/TicketContainer.jsx
+++ src/Pages/TicketContainer/TicketContainer.jsx
@@ -1,4 +1,5 @@
 import { use } from "react";
+import Ticket from "../../Components/Ticket";
 export default function TicketContainer({ TicketPromise }) {
   const ticketData = use(TicketPromise);
   const tickets = ticketData.tickets;
@@ -8,38 +9,14 @@ export default function TicketContainer({ TicketPromise }) {
       {/* All ticket card */}
       <div className="grid flex-1 md:grid-cols-2 ">
         {tickets.map((ticket) => (
-          <div key={ticket.id}>
-            <div>
-              {/* start */}
-              <div className="shadow-sm card bg-base-100 w-96">
-                <div className="card-body">
-                  <div className="flex justify-between">
-                    <h2 className="font-bold card-title">{ticket?.title}</h2>
-                    <span className="badge badge-secondary">
-                      {ticket?.status}
-                    </span>
-                  </div>
-                  <p>{ticket?.description}</p>
-                  <div className="justify-between card-actions">
-                    <div className=" badge badge-outline">
-                      <span>#{ticket?.id}</span> {ticket?.priority} priority
-                    </div>
-                    <div className=" badge badge-outline">
-                      {ticket?.customer} <span>{ticket?.createdAt}</span>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              {/* end */}
-            </div>
-          </div>
+          <Ticket key={ticket.id} ticket={ticket}></Ticket>
         ))}
       </div>
 
       {/* task status */}
       <div className="px-20 ">
         {/* add to inprogress */}
-        <div>
+        <div className="p-10`">
           <h3>Task Status</h3>
           <p>select a ticket to add to Task Status</p>
         </div>
