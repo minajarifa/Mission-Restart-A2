597b0ffd4e9710e829334410b773945b2ef18c18
Your Name <minajmiha02@gmail.com>
2026-03-10 14:19:11 +0600

    end

--- src/Layout/Banner/Banner.jsx
+++ src/Layout/Banner/Banner.jsx
@@ -2,25 +2,25 @@ export default function Banner({progress,resolved}) {
   return (
     <div className="justify-center lg:flex">
       <div className="flex bg-gradient-to-r from-[#632EE3] to-[#9F62F2] rounded-2xl m-10">
-        <img src="/public/vector1.png" alt="" className="" />
+        <img src="/vector1.png" alt="" className="" />
         <div className="mt-20 text-center text-white">
           <h1 className="font-bold ">In-Progress</h1>
           <h1 className="text-3xl">{progress?.length}</h1>
         </div>
         <img
-          src="/public/vector1.png"
+          src="/vector1.png"
           alt=""
           className=" transform scale-x-[-1] "
         />
       </div>
       <div className="bg-gradient-to-r from-[#54CF68] to-[#00827A] flex rounded-2xl m-10">
-        <img src="/public/vector1.png" alt="" className="" />
+        <img src="/vector1.png" alt="" className="" />
         <div className="mt-20 text-center text-white">
           <h1 className="font-bold">Resolved</h1>
           <h1 className="text-3xl">{resolved?.length}</h1>
         </div>
         <img
-          src="/public/vector1.png"
+          src="/vector1.png"
           alt=""
           className=" transform scale-x-[-1]"
         />
